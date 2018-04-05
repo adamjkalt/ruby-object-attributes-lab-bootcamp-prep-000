501682877cb1441bacc320e2819838b81bc9c60a
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-05 02:40:20 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,19 @@
 class Person
-end
+
+def name=(person_name)
+   @this_persons_name = persons_name
+ end
+
+ def name
+   @this_persons_name
+ end
+
+ def job=(persons_job)
+    @this_persons_job = persons_job
+  end
+
+  def job
+    @this_persons_jobs
+  end
+ end
+
